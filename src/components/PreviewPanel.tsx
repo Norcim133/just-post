409e23673777c209a3e7e0a3c624fd607d4ce8e3
Norcim133 <ian.hill@gmail.com>
2025-07-05 09:34:22 +1000

    Scroll preview on overflow

--- src/components/PreviewPanel.tsx
+++ src/components/PreviewPanel.tsx
@@ -9,21 +9,21 @@ interface PreviewPanelProps {
 const PreviewPanel = ({ selectedPlatforms, postText }: PreviewPanelProps ) => {
     return (
         <div className="w-96 bg-white shadow-sm border-r border-slate-100 flex flex-col" style={{ fontFamily: '-apple-system, BlinkMacSystemFont, "Segoe UI", system-ui, sans-serif'}}>
-        <div className="flex-1 px-6 pb-4">
+            <div className="flex-1 px-6 pb-4 overflow-y-auto">
 
-        <h3 className="pt-10 text-sm font-semibold text-slate-700 mb-4 tracking-wide uppercase">Post Previews</h3>
-          <hr className="pt-5 border-gray-300" />
+                <h3 className="pt-10 text-sm font-semibold text-slate-700 mb-4 tracking-wide uppercase">Post Previews</h3>
+                <hr className="pt-5 border-gray-300" />
 
-        {selectedPlatforms.map(platformState => (
-          <PlatformPreview
-          key={platformState.id}
-          text={postText}
-          platformConfig={platformState.config}
-          />
-        ))}
+                {selectedPlatforms.map(platformState => (
+                    <PlatformPreview
+                    key={platformState.id}
+                    text={postText}
+                    platformConfig={platformState.config}
+                    />
+                ))}
 
-      </div>
-    </div>
+            </div>
+        </div>
 
     )
 }
