4ebea14b8ee12cd642ec746da73a549381af8b38
Norcim133 <ian.hill@gmail.com>
2025-07-08 16:47:46 +1000

    Move to postgres for auth db

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,11 +1,15 @@
 // src/lib/auth.ts
 
 import { betterAuth } from "better-auth";
-import Database from "better-sqlite3";
+import { Pool } from "pg";
+
+const pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+});
 
 // This is the core server-side instance of Better Auth.
 export const auth = betterAuth({
-    database: new Database("./sqlite.db"),
+    database: pool,
     baseURL: "http://localhost:3000",
     // Enable the email and password authenticator
     emailAndPassword: {
